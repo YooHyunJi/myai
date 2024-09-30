0db6b2240be999c118c40a89ac7e4b6a7fc172dd
linzy <ccamy0915@naver.com>
2024-09-30 16:22:16 +0900

    AI chat bot

--- src/main/java/org/springframework/ai/openai/samples/helloworld/AIController.java
+++ src/main/java/org/springframework/ai/openai/samples/helloworld/AIController.java
@@ -1,6 +1,7 @@
 package org.springframework.ai.openai.samples.helloworld;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins="*")
 class AIController {
 	private final ChatClient chatClient;
 
